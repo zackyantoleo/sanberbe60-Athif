f125fcb7cca73d9af2ea5043ad5b577de171ed30
zackyantoleo <zackyanto.leo@gmail.com>
2024-10-18 18:46:54 +0700

    update swagger-ui

--- src/docs/route.ts
+++ src/docs/route.ts
@@ -1,22 +1,17 @@
 import { Express } from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerOutput from "./swagger_output.json";
-import fs from "fs";
 import path from "path";
 
 export default function docs(app: Express) {
-  const css = fs.readFileSync(
-    path.resolve(
-      __dirname,
-      "../../node_modules/swagger-ui-dist/swagger-ui.css"
-    ),
-    "utf-8"
-  );
-  app.use(
-    "/docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerOutput, {
-      customCss: css,
-    })
-  );
+  const options = {
+    customCssUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui-bundle.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui-standalone-preset.js",
+    ],
+  };
+
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput, options));
 }
